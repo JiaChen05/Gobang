1afc8b29698e9fcd31f0f17022d1e2cfd85e81e3
JiaChen05 <jc8773@nyu.edu>
2018-09-11 12:06:12 -0400

    'gobang'

---
+++ resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by gobang.rc
+//
+#define IDR_MENU                        101
+#define IDI_ICON                        102
+#define IDC_VISION                      40002
+#define IDC_SERVER                      40003
+#define IDC_CONNECT                     40004
+#define IDC_SINGLE                      40006
+#define IDC_MULTI                       40007
+#define IDC_EXIT                        40008
+#define IDC_ABORT                       40009
+#define IDC_DOUBLE                      40010
+#define IDC_RESET                       40011
+#define IDC_FAIL                        40012
+#define IDC_CUT                         40013
+#define IDC_DOUBLEEND                   40014
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40015
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
